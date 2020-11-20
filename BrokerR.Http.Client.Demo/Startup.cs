196512bf3448fd6b516a93a322cf7e0e70388a5d
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2020-11-20 09:37:45 +0100

    Removes JetBrains.Annotations

--- BrokerR.Http.Client.Demo/Startup.cs
+++ BrokerR.Http.Client.Demo/Startup.cs
@@ -26,8 +26,8 @@ namespace BrokerR.Http.Client.Demo
             var connectionString = Configuration.GetConnectionString("BrokerR");
             var crmOptions = Configuration.GetSection(CRMOptions.CRM).Get<CRMOptions>();
 
-            services.AddBrokerR(connectionString, (s, broker) => BrokerROptionsExtensions.WithCRM(broker
-                        .ConfigureLogging(BrokerRLogLevel.Debug), crmOptions, crm => crm
+            services.AddBrokerR(connectionString, (s, broker) => broker
+                .ConfigureLogging(BrokerRLogLevel.Debug).WithCRM(crmOptions, crm => crm
                     .WithEntity("contacts", ContactChanged.Create)
                     .WithEntity("accounts", AccountChanged.Create)
                 )
